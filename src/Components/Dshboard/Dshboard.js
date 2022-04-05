52ba10ce65fda7c5eb464ce02fd955fa8f5f76fa
nuralmumit <nuralmumit@kali.nuralmumit>
2022-04-05 19:47:19 +0600

    added Cheart

--- src/Components/Dshboard/Dshboard.js
+++ src/Components/Dshboard/Dshboard.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import "./Dshboard.css"
 
 const Dshboard = () => {
-    return (
-        <div>
-            <h1>this is Dsahboard</h1>
-        </div>
-    );
+  const [item, setItem] = useState([]);
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+  }, []);
+  return (
+    <div>
+      <h1>Show our progress on chart</h1>
+      <div className="cheart-container">
+      <div className="line-chart">
+        <LineChart className="chart" width={500} height={400} data={item}>
+          <Line dataKey={"sell"}></Line>
+          <Line dataKey={"revenue"}></Line>
+          <Line dataKey={"investment"}></Line>
+          <XAxis dataKey={"month"}></XAxis>
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
+        </LineChart>
+      </div>
+      <div>
+        <AreaChart width={500} height={400} data={item}>
+          <CartesianGrid />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="sell"
+            stackId="1"
+            stroke="#8884d8"
+            fill="#8884d8"
+          />
+          <Area
+            type="monotone"
+            dataKey="revenue"
+            stackId="1"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+          />
+          <Area
+            type="monotone"
+            dataKey="investment"
+            stackId="1"
+            stroke="#ffc658"
+            fill="#ffc658"
+          />
+        </AreaChart>
+      </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dshboard;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,13 +5,13 @@ import './Header.css'
 const Header = () => {
     return (
     <div>
+        <h1>Welcome to your Book Shop </h1>
         <nav className='nav-container'>
             <Link to="/home">Home</Link>
             <Link to="/Reviews">Reviews</Link>
             <Link to="/Dshboard">Dshboard</Link>
             <Link to="/About">About</Link>
             </nav>
-            <h1>Welcome to your Book Shop </h1>
             </div>        
     );
 };
