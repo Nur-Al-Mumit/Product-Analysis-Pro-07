a1c17514a53529c497fdaa35c080f4b370ac14d3
nuralmumit <nuralmumit@kali.nuralmumit>
2022-04-17 11:07:31 +0600

    add some new data on reviews

--- src/Components/ReviewDetails/ReviewDetails.js
+++ src/Components/ReviewDetails/ReviewDetails.js
@@ -9,12 +9,14 @@ const ReviewDetails = ({ data }) => {
     reviewed_at,
     review_country,
     profile,
+    rating,
   } = data;
     // console.log(data);
   return (
     <div className="review-cart">
       <img className="profile-img" src={profile} alt="img"></img>
       <h1>{profile_name}</h1>
+      <h3>Rating: {rating}</h3>
       <h3>{review_title}</h3>
       <p>{review_text.length > 300 ? review_text.slice(0, 300) + "...": review_text}</p>
       <p>{review_country}</p>
