c9667b23cbb9865aa3383932c15a096ac50b0dd8
nuralmumit <nuralmumit@kali.nuralmumit>
2022-04-17 10:51:24 +0600

    added error handaling file

--- src/Components/ReviewDetails/ReviewDetails.js
+++ src/Components/ReviewDetails/ReviewDetails.js
@@ -16,7 +16,7 @@ const ReviewDetails = ({ data }) => {
       <img className="profile-img" src={profile} alt="img"></img>
       <h1>{profile_name}</h1>
       <h3>{review_title}</h3>
-      <p>{review_text}</p>
+      <p>{review_text.length > 300 ? review_text.slice(0, 300) + "...": review_text}</p>
       <p>{review_country}</p>
       <p>{reviewed_at}</p>
     </div>
